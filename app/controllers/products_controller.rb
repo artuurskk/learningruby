17c8970445c1fac8b0e872cad8cd802b41ca3f8c
Artūrs <artuurskk@inbox.lv>
2019-01-10 11:10:22 +0200

    Validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,18 +25,20 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to @product,
+                                  notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created,
+                             location: @product }
       else
+        puts @product.errors.full_messages
         format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors,
+                             status: :unprocessable_entity }
       end
     end
   end
-
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
