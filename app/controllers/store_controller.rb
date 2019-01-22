e1a0bd59dbb4e8a9dfb94949b03ae3a2c258f515
Artūrs <artuurskk@inbox.lv>
2019-01-22 09:35:54 +0200

    done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -10,11 +10,11 @@ class StoreController < ApplicationController
       redirect_to store_index_url(locale: params[:set_locale])
 
     else if params[:search]
-          @products = Product.search(params[:search]).order("created_at DESC")
+          @products = Product.paginate(:page => params[:page], :per_page => 10).search(params[:search]).order("created_at DESC")
         else if params[:category_id]
-           @products = Product.where(:category_id => params[:category_id])
+           @products = Product.paginate(:page => params[:page], :per_page => 10).where(:category_id => params[:category_id])
              else
-              @products = Product.paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
+              @products = Product.paginate(:page => params[:page], :per_page => 3).order("created_at DESC")
           end
         end
     end
